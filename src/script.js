2df30bb2a961c1bb5c1e1ee270757283c1176c3e
es5es5 <es5e5s@nate.com>
2023-05-18 18:23:11 +0900

    17:40

--- src/script.js
+++ src/script.js
@@ -1,3 +1,19 @@
 var canvas = document.getElementById('canvas1');
 var ctx = canvas.getContext('2d');
-console.log(ctx);
+var CANVAS_WIDTH = canvas.width = 600;
+var CANVAS_HEIGHT = canvas.height = 600;
+var playerImage = new Image();
+playerImage.src = './assets/shadow_dog.png';
+var spriteWidth = 575;
+var spriteHeight = 523;
+var frameX = 0;
+var frameY = 0;
+var x = 0;
+function animate() {
+    ctx === null || ctx === void 0 ? void 0 : ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    // ctx?.fillRect(100, 50, 100, 100)
+    // ctx?.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh)
+    ctx === null || ctx === void 0 ? void 0 : ctx.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+    requestAnimationFrame(animate);
+}
+animate();
--- src/script.ts
+++ src/script.ts
@@ -1,3 +1,24 @@
 const canvas = document.getElementById('canvas1') as HTMLCanvasElement
 const ctx = canvas.getContext('2d')
-console.log(ctx)
+const CANVAS_WIDTH = canvas.width = 600
+const CANVAS_HEIGHT = canvas.height = 600
+
+const playerImage = new Image()
+playerImage.src = './assets/shadow_dog.png'
+
+const spriteWidth = 575
+const spriteHeight = 523
+
+let frameX = 0;
+let frameY = 0;
+
+let x = 0
+function animate() {
+  ctx?.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+  // ctx?.fillRect(100, 50, 100, 100)
+  // ctx?.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh)
+  ctx?.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight)
+  requestAnimationFrame(animate)
+}
+
+animate()
